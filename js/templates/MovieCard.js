5ecca38531f620ca8e086b884998ccc27f170daa
bayous3 <sebia.baya@outlook.com>
2023-12-12 22:27:27 +0100

    update

---
+++ js/templates/MovieCard.js
@@ -0,0 +1,32 @@
+class MovieCard {
+    constructor(movie) {
+        this._movie = movie
+
+        this.$wrapper = document.createElement('div')
+        this.$wrapper.classList.add('movie-card-wrapper')
+    }
+
+    get movie() {
+        return this._movie
+    }
+
+    createMovieCard() {
+        const movieCard = `
+            <div class="movie-thumbnail center">
+                <img
+                    alt="${this._movie.title}"
+                    src="${this._movie.thumbnail}"
+                />
+            </div>
+            <h3 class="fs-16 center">${this._movie.title}</h3>
+            <p class="fs-14 center">
+                <span>${this._movie.released_in}</span>
+                -
+                <span>${this._movie.duration}</span>
+            </p>
+        `
+        
+        this.$wrapper.innerHTML = movieCard
+        return this.$wrapper
+    }
+}
